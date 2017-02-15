b79cd728e0aa9348ece545709908bf2cc7e766a1
Daniel Achleitner <daniel.achleitner@gmail.com>
2017-02-15 22:04:51 +0100

    Fix missing TLS cert authorities

--- torrents/index.sh
+++ torrents/index.sh
@@ -1,3 +1,3 @@
-#!/bin/bash -eu
+#!/bin/bash -euv
 find . -name "*.torrent" -print0 | xargs -0 aria2c -S | gzip > torrents.txt.gz
 zcat torrents.txt.gz | grep ^Name | sort | gzip > torrent-names.txt.gz
